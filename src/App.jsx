ec4ec6b61614cffce7c435ca738bc8cfb2bcc4aa
Angel Garcia <angel.garc.ort@gmail.com>
2024-04-11 22:26:14 -0600

    ejemplo de drag and drop

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+import {useEffect, useState } from "react";
+
+const initialTodos = JSON.parse(localStorage.getItem('todos')) || [
+  { id: 1, text: "bañarse" },
+  { id: 2, text: "jalada de pito" },
+  { id: 3, text: "cenar" },
+];
+
+const App = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  useEffect(()=>{
+    localStorage.setItem('todos',JSON.stringify(todos))
+  },[todos])
+
+  const handleDragEnd =(result) =>{
+    if(!result.destination) return;
+    const startIndex = result.source.index;
+    const endIndex = result.destination.index;
+
+    const copyArray = [...todos];
+    const [reorderedItem] = copyArray.splice(startIndex, 1);
+
+    copyArray.splice(endIndex, 0, reorderedItem);
+    setTodos(copyArray)
+  }
+
+  return (
+    <>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <h1>Todo app</h1>
+        <Droppable droppableId="todos" >
+          {(droppableProvider) => (
+            <ul
+              ref={droppableProvider.innerRef}
+              {...droppableProvider.droppableProps}
+            >
+              {todos.map((todo, index) => (
+                <Draggable
+                  index={index}
+                  key={todo.id}
+                  draggableId={`${todo.id}`}
+                >
+                  {(draggeableProvider) => (
+                    <li
+                      ref={draggeableProvider.innerRef}
+                      {...draggeableProvider.dragHandleProps}
+                      {...draggeableProvider.draggableProps}
+                    >
+                      {todo.text}
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {droppableProvider.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </>
+  );
+};
+
+export default App;
